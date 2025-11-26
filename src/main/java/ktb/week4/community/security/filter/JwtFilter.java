1b17eb3c8cfa15fa78234691d2b4ba2c97dafd87
tl1l1l1s <s2oo1og24@gmail.com>
2025-11-26 15:26:31 +0900

    Fix: Token 형식이 잘못되어 있을 때 허용된 url에도 접근 불가능했던 오류 수정

--- src/main/java/ktb/week4/community/security/filter/JwtFilter.java
+++ src/main/java/ktb/week4/community/security/filter/JwtFilter.java
@@ -1,15 +1,10 @@
 package ktb.week4.community.security.filter;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import ktb.week4.community.global.apiPayload.ApiResponse;
-import ktb.week4.community.global.apiPayload.ErrorCode;
 import ktb.week4.community.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -21,45 +16,28 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
 	
-	private final ObjectMapper objectMapper;
 	private final JwtTokenProvider jwtTokenProvider;
 	
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-	
+		
 		Cookie[] cookies = request.getCookies();
-		if(cookies != null && Arrays.stream(cookies).anyMatch(cookie -> cookie.getName().equals("accessToken"))) {
-			String accessToken = Arrays.stream(cookies)
-					.filter(cookie -> cookie.getName().equals("accessToken"))
-					.map(Cookie::getValue)
-					.findFirst()
-					.orElse(null);
-
-			try {
-				jwtTokenProvider.validateToken(accessToken);
+		try {
+			if (cookies != null && Arrays.stream(cookies).anyMatch(cookie -> cookie.getName().equals("accessToken"))) {
+				String accessToken = Arrays.stream(cookies)
+						.filter(cookie -> cookie.getName().equals("accessToken"))
+						.map(Cookie::getValue)
+						.findFirst()
+						.orElse(null);
 				
+				jwtTokenProvider.validateToken(accessToken);
 				Authentication auth = jwtTokenProvider.resolveToken(accessToken);
 				SecurityContextHolder.getContext().setAuthentication(auth);
 			}
-			catch (ExpiredJwtException e) {
-				response.setStatus(401);
-				response.setContentType("application/json;charset=UTF-8");
-				response.getWriter().write(objectMapper.writeValueAsString(
-						ApiResponse.onFailure(ErrorCode.TOKEN_EXPIRED.getCode(),  ErrorCode.TOKEN_EXPIRED.getMessage())
-				));
-				return;
-			}
-			catch (JwtException e) {
-				response.setStatus(401);
-				response.setContentType("application/json;charset=UTF-8");
-				response.getWriter().write(objectMapper.writeValueAsString(
-						ApiResponse.onFailure(ErrorCode.UNAUTHORIZED_REQUEST.getCode(),  ErrorCode.UNAUTHORIZED_REQUEST.getMessage())
-				));
-				return;
-			}
+		} catch (Exception e) {
+			SecurityContextHolder.clearContext();
 		}
 		
 		filterChain.doFilter(request, response);
 	}
-	
 }
